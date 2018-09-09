1708ffe4217138d6021fafd79e7b86d4143e12ac
jsumedh10 <jsumedh10@gmail.com>
2018-09-09 14:25:27 +0530

    added route for linkedlists.html

--- FlaskApp.py
+++ FlaskApp.py
@@ -8,5 +8,10 @@ def home():
     return render_template('basic.html')
 
 
+@app.route('/linkedlists/')
+def linkedlists():
+    return render_template('LinkedLists.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
