2b933cd750e00c20d01825df6f2095087c7feed4
jsumedh10 <jsumedh10@gmail.com>
2018-09-12 18:31:50 +0530

    updtated Node Class definition

--- PythonFiles/stacks.py
+++ PythonFiles/stacks.py
@@ -2,7 +2,26 @@
 This file implements stacks using singly linked lists.
 """
 
-from LinkedLists import Node
+
+class Node(object):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, data):
+        self.data = data
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, next):
+        self.next = next
+
+    def has_next(self):
+        return self.next is not None
 
 
 class Stack(object):
