408b0251f2abb56c051d27e7ace1b69757776418
jsumedh10 <jsumedh10@gmail.com>
2018-09-14 23:58:10 +0530

    added class definition

--- PythonFiles/trees.py
+++ PythonFiles/trees.py
@@ -1,2 +1,21 @@
-def tree():
-    pass
+class BinaryTreeNode:
+    def __init__(self, data, left=None, right=None):
+        # data for root node
+        self.data = data
+        # left child
+        self.left = left
+        # right child
+        self.right = right
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, data):
+        self.data = data
+
+    def get_left(self):
+        return self.left
+
+    def get_right(self):
+        return self.right
+
