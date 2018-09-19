4b7f8c70c0d3da6b40108e36e89d0ec4530ccf9d
jsumedh10 <jsumedh10@gmail.com>
2018-09-20 00:19:38 +0530

    adding trees.py

--- PythonFiles/trees.py
+++ PythonFiles/trees.py
@@ -1,4 +1,9 @@
-class BinaryTreeNode:
+# implementation of binary search tree
+# where all left nodes are smaller than root
+# and all right nodes are greater than root
+
+
+class BinarySearchTreeNode:
     def __init__(self, data, left=None, right=None):
         # data for root node
         self.data = data
@@ -19,3 +24,59 @@ class BinaryTreeNode:
     def get_right(self):
         return self.right
 
+    def insert(self, data):
+
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = BinarySearchTreeNode(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = BinarySearchTreeNode(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def pre_order_traversal(self, root):
+        result = []
+        if root:
+            result.append(root.data)
+            result = result + self.pre_order_traversal(root.get_left())
+            result = result + self.pre_order_traversal(root.get_right())
+
+        return result
+
+    def in_order_traversal(self, root):
+        result = []
+        if root:
+            result = self.in_order_traversal(root.get_left())
+            result.append(root.data)
+            result = result + self.in_order_traversal(root.get_right())
+
+        return result
+
+    def post_order_traversal(self, root):
+        result = []
+        if root:
+            result = self.post_order_traversal(root.get_left())
+            result = result + self.post_order_traversal(root.get_right())
+            result.append(root.data)
+
+        return result
+
+
+root = BinarySearchTreeNode(27)
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+
+
+print(root.pre_order_traversal(root))
+print(root.in_order_traversal(root))
+print(root.post_order_traversal(root))
