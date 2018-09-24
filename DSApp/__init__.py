fcb41b75e11ced53d8bdcd8afe7b4dc32749badb
jsumedh10 <jsumedh10@gmail.com>
2018-09-24 12:00:09 +0530

    DSApp now has __init__

--- DSApp/__init__.py
+++ DSApp/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 DSApp = Flask(__name__)
+DSApp.config.from_object(Config)
+db = SQLAlchemy(DSApp)
+migrate = Migrate(DSApp, db)
 
-from DSApp import routes
+from DSApp import routes, models
